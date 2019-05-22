3882a797ce764b99d729ab7467f904a6b93daf47
yanweicai <yanweicai@moqihuyu.com>
2019-05-22 10:24:59 +0800

    添加分开导出功能

--- FbxExporter/Assets/UTJ/FbxExporter/Editor/FbxExporterWindow.cs
+++ FbxExporter/Assets/UTJ/FbxExporter/Editor/FbxExporterWindow.cs
@@ -31,11 +31,22 @@ namespace UTJ.FbxExporter
             public string path;
             public DateTime started = DateTime.Now;
         }
+
+        // ¼ÇÂ¼µÈ´ýµ¼³ö¶ÔÏó
+        static HashSet<GameObject> waitExportObjects;
+        //ÓÃÓÚÈ¥ÖØ
+        static Dictionary<string, int> nameMaps = new Dictionary<string, int>();
+        static string exportPath;
+
         static List<Record> s_records = new List<Record>();
+
+        // ÊÇ·ñÕýÔÚÖ´ÐÐupdateº¯Êý
+        static bool m_isPollAsync = false;
 
         Scope m_scope = Scope.Selected;
         bool m_includeChildren = true;
-        FbxExporter.Format m_format = FbxExporter.Format.FbxBinary;
+        bool m_isSeperateExport = false;
+        static FbxExporter.Format m_format = FbxExporter.Format.FbxBinary;
         FbxExporter.ExportOptions m_opt = FbxExporter.ExportOptions.defaultValue;
 
         bool DoExport(string path, FbxExporter.Format format, GameObject[] objects)
@@ -50,8 +61,10 @@ namespace UTJ.FbxExporter
             {
                 Debug.Log("Export started: " + path);
                 s_records.Add(new Record {path = path, exporter = exporter});
-                if (s_records.Count == 1)
+                if (m_isPollAsync == false) { 
                     EditorApplication.update += PollAsyncWrite;
+                    m_isPollAsync = true;
+                }
                 return true;
             }
             else
@@ -70,16 +83,97 @@ namespace UTJ.FbxExporter
                 if (record.exporter.IsFinished())
                 {
                     var elapsed = DateTime.Now - record.started;
-                    record.exporter.Release();
                     Debug.Log("Export finished: " + record.path + " (" + elapsed.TotalSeconds + " seconds)");
                     finished = true;
                 }
             }
             if (finished)
             {
-                s_records.RemoveAll((a) => { return a.exporter.IsFinished(); });
-                if (s_records.Count == 0)
+                s_records.RemoveAll((a) => {
+                    var isFinished = a.exporter.IsFinished();
+                    if (isFinished) { 
+                        a.exporter.Release();
+                    }
+                    return isFinished;
+                });
+
+                if (s_records.Count == 0 && m_isPollAsync == true) { 
                     EditorApplication.update -= PollAsyncWrite;
+                    m_isPollAsync = false;
+                }
+            }
+        }
+
+        void addExportTask(GameObject taskObject) {
+            string filename;
+
+            string extension = "fbx";
+            if (m_format == FbxExporter.Format.Obj) { extension = "obj"; }
+
+            //²éÕÒmesh
+            var meshFilterComponent = taskObject.GetComponent<MeshFilter>();
+            if (meshFilterComponent && meshFilterComponent.sharedMesh.vertexCount > 0)
+            {
+                filename = meshFilterComponent.sharedMesh.name;
+
+                // È¥µô" Instance"
+                filename = filename.Replace(" Instance", "");
+
+                //ÅÐ¶ÏÊÇ·ñµ¼³ö¹ý
+                var isExported = nameMaps.ContainsKey(filename);
+
+                if (isExported == false)
+                {
+                    nameMaps.Add(filename, 1);
+
+                    var tmpPath = exportPath.Substring(0, exportPath.LastIndexOf("/") + 1) + filename + "." + extension;
+                    //Debug.Log(tmpPath);
+
+                    var tmpObjects = new HashSet<GameObject>();
+                    tmpObjects.Add(taskObject.gameObject);
+                    DoExport(tmpPath, m_format, tmpObjects.ToArray());
+                }
+                else
+                {
+                    Debug.Log(filename);
+                }
+            }
+        }
+
+        void PollAsyncAddExportTask()
+        {
+            // ÅÐ¶Ïµ±Ç°Ö´ÐÐµÄÈÎÎñÊýÁ¿
+            var currentTaskCount = s_records.Count;
+            if (currentTaskCount < 1) {
+                var needAddCount = 1 - currentTaskCount;
+
+                // ¼ÇÂ¼ÐèÒªÒÆ³ýµÄkey
+                var needDelKets = new List<GameObject>();
+
+                // Ìí¼ÓÈÎÎñ
+                foreach (var taskObject in waitExportObjects)
+                {
+                    addExportTask(taskObject.gameObject);
+                    needDelKets.Add(taskObject);
+
+                    needAddCount--;
+                    if (needAddCount <= 0)
+                    {
+                        break;
+                    }
+                }
+
+                // ÒÆ³ýÌí¼Ó¹ýµÄÈÎÎñ
+                foreach (var needDelObj in needDelKets) {
+                    waitExportObjects.Remove(needDelObj);
+                }
+            }
+
+            //ÅÐ¶ÏÊÇ·ñ»¹Ê£ÏÂÈÎÎñ
+            if ( waitExportObjects.Count == 0)
+            {
+                EditorApplication.update -= PollAsyncAddExportTask;
+                Debug.Log("Ìí¼ÓÈÎÎñ½áÊø.");
             }
         }
 
@@ -99,8 +193,9 @@ namespace UTJ.FbxExporter
 
             EditorGUILayout.Space();
 
-            m_opt.transform = EditorGUILayout.Toggle("Include Transform", m_opt.transform);
+            m_isSeperateExport = EditorGUILayout.Toggle("Seperate Export", m_isSeperateExport);
 
+            m_opt.transform = EditorGUILayout.Toggle("Include Transform", m_opt.transform);
             m_opt.flip_handedness = EditorGUILayout.Toggle("Flip Handedness", m_opt.flip_handedness);
             m_opt.flip_faces = EditorGUILayout.Toggle("Flip Faces", m_opt.flip_faces);
             m_opt.quadify = EditorGUILayout.Toggle("Quadify", m_opt.quadify);
@@ -166,11 +261,20 @@ namespace UTJ.FbxExporter
                         }
                     }
 
-
                     var path = EditorUtility.SaveFilePanel("Export ." + extension + " file", "", SanitizeForFileName(filename), extension);
                     if (path != null && path.Length > 0)
                     {
-                        DoExport(path, m_format, objects.ToArray());
+                        if (m_isSeperateExport)
+                        {
+                            exportPath = path;
+                            waitExportObjects = objects;
+
+                            EditorApplication.update += PollAsyncAddExportTask;
+                        }
+                        else
+                        {
+                            DoExport(path, m_format, objects.ToArray());
+                        }
                     }
                 }
             }
